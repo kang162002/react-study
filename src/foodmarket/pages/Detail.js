71dab1b1850a053ee1bf2393a479bf90495461c8
kang162002 <6334540@naver.com>
2025-05-27 14:22:33 +0900

    스타일 적용 방법(객체,css, classname)

--- src/foodmarket/pages/Detail.js
+++ src/foodmarket/pages/Detail.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router';
 import { Container, Row, Col, Button, Modal } from "react-bootstrap";
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { responsivePropType } from 'react-bootstrap/esm/createUtilityClasses';
 
 
 
@@ -16,14 +17,16 @@ function Detail({ foods }) {
     let [orderCount, setOrderCount] = useState(0);
     //  foods 전체 정보 보유
 
-    let [showModal, setShowModal] = useState(true);
+    let [showModal, setShowModal] = useState(false);
+
+    let [viewClass, setViewClass] = useState('');
 
 
     //  id  : 현재 페이지에서 표시해야하는 food의 id가 확인됨
 
     // foods에 들어있는 id 값 == 주소 params 에 들어있는 id 값
     let food = foods.find((food) => {
-        return food.id === id;
+        return food.id == id;
     })
 
     //food.findIndex
@@ -71,17 +74,26 @@ function Detail({ foods }) {
 */
 
     // modal창 가리기
-    useEffect(()=>{
-        
+    useEffect(() => {
+
         setTimeout(() => {
-                 setShowModal(false);
+            setShowModal(false);
         }, 2000);
-        
-       
-
-
 
     }, [])
+
+    // conatiner opacity를 이용한 트랜지션 효과
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            setViewClass('end'); // '' > 'end'
+            // 'start ' > 'start end'
+        }, 2);
+
+    }, [])                   
+
+
 
 
 
@@ -93,20 +105,107 @@ function Detail({ foods }) {
             <div><h1>존재하지 않는 상품입니다. 잘못된 접근입니다</h1></div>
         )
 
+
+    }
+
+
+    // style 객체 단위로 관리
+    const tempStyle = {
+        color: "orange",
+        fontSize: '20px'
+    };
+
+    const blueTextStyle = {
+        color: "blue"
+    }
+
+    const styles = {
+        redStyle: {
+            color: "red"
+        },
+        blueStyle: {
+            color: "blue"
+        },
+        fontBigBold: {
+            fontSize: "2rem",
+            fontWeight: "bold"
+        }
     }
 
 
 
+
+    //  가격 표시( food.price)
+    /* 
+    1만원 이상 > 빨산색
+    1만원 미만 > 파란색
+
+        food.price >= 10000
+
+    */
+
+    const priceTextStyle = {
+        color: food.price >= 10000 ? 'red' : 'blue'
+    }
+    //  <p style={priceTextStyle}>{food.price}</p>
+    //  <p style={{color: food.price >= 10000 ? 'red' : 'blue' }}>{food.price}</p>}}</p>
+
+    //2) js 함수 형태
+
+    const priceTextStyleFunc = (price) => {
+
+        if (price >= 10000)
+            return { color: "red" }
+        else
+            return { color: "blue" }
+        //  
+    }
+    //    <p style={priceTextStyleFunc(food.price)}>{food.price}</p> 
+
+    /*
+        Css 클래스명 연계활용
+    
+        // 단일 클래스 적용 
+        <p  className={food.price >= 10000 ? 'price-red' : 'price-blue'}>{food.price}</p>
+    
+        //  다중 클래스 적용 
+        <p  className={food.price +10000 ? 'price-red text-strong' : 'price-blue  text-strong')}>{food.price}</p>
+        <p  className={food.price +10000 ? 'price-red text-strong' : 'price-blue  text-strong')}>{food.price}</p>
+        <p  className={'text-strong' +  (food.praice > = 10000 ? 'price-red : 'price-blue</p>)  }</p>
+
+        + 연산
+        <p className={ ' text-strong' + (food.price >=10000 ? 'price-red' : ' price-blue')}</p>)}
+
+        // Join 함수
+
+        <p className={['text-Strong', food.price >= 1000 ? 'price-red' : 'price-blue;].join(" ")}>{food.price}</p>
+    
+        변수
+        const priceClassName = 'text-strong price-red';
+
+        백틱문자 활용 `
+        <p className={`text-strong ${food.price >= 10000 ? 'price-red' : 'priceblue'}`}>{food.price}</p>
+
+
+
+        */
+
+
+
     return (
-        <Container>
+        //  "start end"
+        <Container className={'start ' + viewClass}>
             <Row>
                 <Col md={6}>
                     <img src={process.env.PUBLIC_URL + food.imgPath} width="100%" />
                 </Col>
                 <Col md={6}>
                     <h4 style={{ paddingTop: '30px' }}>{food.title}</h4>
-                    <p>{food.content}</p>
-                    <p>{food.price}</p>
+                    {/* <p style={tempStyle}>{food.content}</p> */}
+                    <p style={styles.fontBigBold}>{food.content}</p>
+
+                    <p className={['text-Strong', food.price >= 10000 ? 'price-red' : 'price-blue'].join(" ")}>{food.price}</p>
+
                     <p>
                         <Button variant="dark" onClick={() => {
                             if (orderCount > 0)
@@ -123,17 +222,17 @@ function Detail({ foods }) {
             </Row>
 
             {
-                    // showModal &&
-                    // showModal == true ? : 
+                // showModal &&
+                // showModal == true ? : 
             }
 
-            <Modal show={showModal} onHide={()=>{setShowModal(false)}}>
+            <Modal show={showModal} onHide={() => { setShowModal(false) }}>
                 <Modal.Header closeButton>
                     <Modal.Title>환영합니다~</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>어서 오세요 ~ Food 구경하세요~</Modal.Body>
                 <Modal.Footer>
-                    <Button variant="primary" onClick={()=>{ setShowModal(false)}}>
+                    <Button variant="primary" onClick={() => { setShowModal(false) }}>
                         확인
                     </Button>
                 </Modal.Footer>
